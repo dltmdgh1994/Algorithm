1073d0e355d7940644306d469fc292badd83bc21
Seungho Lee <60536427+dltmdgh1994@users.noreply.github.com>
2021-06-10 22:56:47 +0900

    Update 0610

--- Programmers/src/Programmers/P12899.java
+++ Programmers/src/Programmers/P12899.java
@@ -6,36 +6,51 @@ public class P12899 { // 124 나라의 숫자
 		System.out.println(p.solution(12));
 	}
 	
+	// 깔끔 코드
 	public String solution(int n) {
-        String answer = "";
-        
-        int num = 1;
-        
-        while(n > num*3) {
-        	num *= 3;
-        	n -= num;
-        }
-        
-        while(num > 0) {
-        	double q = (double)n/(double)num;
-        	
-        	if(n%num == 0) {
-        		n = num;
-        	}else {
-        		n %= num;
-        	}
-        	
-        	num /= 3;
-        	
-        	if(q > 2) {
-        		answer += "4";
-        	}else if(q > 1) {
-        		answer += "2";
-        	}else {
-        		answer += "1";
-        	}
-        }
-        
-        return answer;
-    }
+		String[] num = {"4","1","2"};
+	    String answer = "";
+
+	    while(n > 0){
+	        answer = num[n % 3] + answer;
+	        // n - 1을 통해 자리올림을 안하는 효과
+	        n = (n - 1) / 3;
+	    }
+	    return answer;
+	}
+	
+//	public String solution(int n) {
+//        String answer = "";
+//        
+//        int num = 1;
+//        
+//		  // num은 3의 배수
+//        while(n > num*3) {
+//        	num *= 3;
+//        	n -= num;
+//        }
+//        
+//        while(num > 0) {
+//        	double q = (double)n/(double)num;
+//        	
+//        	if(n%num == 0) {
+//        		n = num;
+//        	}else {
+//        		n %= num;
+//        	}
+//        	
+//        	num /= 3;
+//        	
+//			// q가 소수자리도 갖고 있기 때문에 > 형식으로
+//        	if(q > 2) {
+//        		answer += "4";
+//        	}else if(q > 1) {
+//        		answer += "2";
+//        	}else {
+//        		answer += "1";
+//        	}
+//        }
+//        
+//        return answer;
+//    }
 }
---
+++ Programmers/src/Programmers/P1835.java
@@ -0,0 +1,16 @@
+package Programmers;
+
+class P1835 {
+	public int solution(int n, String[] data) {
+        int answer = 0;
+        
+        String[] str = {"0", "0", "0", "0", "0", "0", "0", "0"};
+        
+        for(int i = 0; i < n; i++) {
+        	
+        }
+        
+        
+        return answer;
+    }
+}
