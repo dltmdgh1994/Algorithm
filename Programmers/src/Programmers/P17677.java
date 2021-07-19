2f60146bf87b3ec9ae67a057d2b632ff71b9b81f
Seungho Lee <60536427+dltmdgh1994@users.noreply.github.com>
2021-07-19 16:41:46 +0900

    뉴스 클러스터링

--- Programmers/src/Programmers/P17677.java
+++ Programmers/src/Programmers/P17677.java
@@ -2,7 +2,7 @@ package Programmers;
 
 import java.util.*;
 
-public class P17677 {
+public class P17677 { // 뉴스 클러스터링
 
 	public static void main(String[] args) {
 		P17677 p = new P17677();
@@ -15,86 +15,64 @@ public class P17677 {
         str1 = str1.toUpperCase();
         str2 = str2.toUpperCase();
         
-        str1 = str1.replaceAll("[^A-Z]", "");
-        str2 = str2.replaceAll("[^A-Z]", "");
-        
-        ArrayList<String> arr = new ArrayList<>();
-        
-        HashSet<String> set1 = new HashSet<>();
-        HashSet<String> set2 = new HashSet<>();
-        HashSet<String> all = new HashSet<>();
-        HashSet<String> common = new HashSet<>();
-        
+        HashMap<String, Integer> set1 = new HashMap<>(); // 첫 문장 단어 및 개수
+        HashMap<String, Integer> set2 = new HashMap<>(); // 다음 문장 단어 및 개수
+        HashSet<String> key = new HashSet<>(); // 문장들의 단어 집합
         
         for(int i = 2; i <= str1.length(); i++) {
         	String str = str1.substring(i-2,i);
         	str = str.replaceAll("[^A-Z]", "");
-        	System.out.println(str);
-        	arr.add(str);
-        }
-        
-        arr.sort(null);
-        
-        int cnt = 1;
-        set1.add(arr.get(0) + Integer.toString(cnt));
-    	all.add(arr.get(0) + Integer.toString(cnt));
-    	
-        for(int i = 1; i < arr.size(); i++) {
-        	String s1 = arr.get(i-1);
-        	String s2 = arr.get(i);
         	
-        	if(s1.equals(s2)) {
-        		s2 += Integer.toString(cnt++);
-        		set1.add(s2);
-            	all.add(s2);
-        	}else if(s1.length() == 2 && s2.length() == 2){
-        		s2 += Integer.toString(cnt);
-        		cnt = 1;
-        		set1.add(s2);
-            	all.add(s2);
+        	if(!set1.containsKey(str) && str.length() == 2) {
+        		set1.put(str, 1);
+        		key.add(str);
+        	}else if(set1.containsKey(str) && str.length() == 2) {
+        		int cnt = set1.get(str) + 1;
+        		set1.put(str, cnt);
         	}
         }
-        
-        arr.clear();
         
         for(int i = 2; i <= str2.length(); i++) {
         	String str = str2.substring(i-2,i);
-        	System.out.println(str);
-        	arr.add(str);
-        }
-        
-        cnt = 1;
-        set2.add(arr.get(0) + Integer.toString(cnt));
-    	all.add(arr.get(0) + Integer.toString(cnt));
-    	
-        for(int i = 1; i < arr.size(); i++) {
-        	String s1 = arr.get(i-1);
-        	String s2 = arr.get(i);
+        	str = str.replaceAll("[^A-Z]", "");
         	
-        	if(s1.equals(s2)) {
-        		s2 += Integer.toString(cnt++);
-        		set2.add(s2);
-            	all.add(s2);
-        	}else if(s1.length() == 2 && s2.length() == 2) {
-        		s2 += Integer.toString(cnt);
-        		cnt = 1;
-        		set2.add(s2);
-            	all.add(s2);
+        	if(!set2.containsKey(str) && str.length() == 2) {
+        		set2.put(str, 1);
+        		key.add(str);
+        	}else if(set2.containsKey(str) && str.length() == 2) {
+        		int cnt = set2.get(str) + 1;
+        		set2.put(str, cnt);
         	}
         }
         
-        for(String str : all) {
-        	if(set1.contains(str) && set2.contains(str)) {
-        		common.add(str);
+        int all = 0;
+        int common = 0;
+        for(String k : key) {
+        	
+        	if(set1.containsKey(k) && set2.containsKey(k)) {
+        		int n1 = set1.get(k);
+        		int n2 = set2.get(k);
+        		
+        		if(n1 >= n2) {
+        			all += n1;
+        			common += n2;
+        		}else {
+        			all += n2;
+        			common += n1;
+        		}
+        	}else if(set1.containsKey(k)) {
+        		all += set1.get(k);
+        	}else if(set2.containsKey(k)) {
+        		all += set2.get(k);
         	}
-        	System.out.println(str);
-        }
-        System.out.println();
-        for(String str : common) {
-        	System.out.println(str);
         }
         
-        double ans = (double)common.size()/all.size();
+        // 특수 문자 때문에 key가 빈 경우
+        if(key.isEmpty()) {
+        	return 65536;
+        }
+        
+        double ans = (double)common/all;
         ans *= 65536;
         
         int answer = (int)ans;
