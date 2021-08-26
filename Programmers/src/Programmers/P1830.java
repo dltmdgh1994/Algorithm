32c441babead1788490d8a63400ab8d61810c6fd
Seungho Lee <60536427+dltmdgh1994@users.noreply.github.com>
2021-08-26 16:06:55 +0900

    3 브라이언의 고민

--- Programmers/src/Programmers/P1830.java
+++ Programmers/src/Programmers/P1830.java
@@ -8,68 +8,80 @@ public class P1830 { // 3 브라이언의 고민
 		
 		P1830 p = new P1830();
 		
-		System.out.println(p.solution("Aaa"));
+		String s = "a";
+		System.out.println(s.substring(2));
+		
+//		System.out.println(p.solution("HaEaLaLaObWORLDb"));
 		
 	}
 
 	// 영문 대문자는 원래 문구, 소문자는 특수기호를 의미
+	
+	// HELLOWORLD
+	// aHELLOaWbObRbLbD
+	// HaEaLaLaObWORLDb
+	// aHbEbLbLbOacWdOdRdLdDc
 	public String solution(String sentence) {
         String answer = "";
         
         HashSet<Character> set = new HashSet<>();
+        ArrayList<String> words = new ArrayList<>();
         
-        boolean upper = false;
-        char rule1 = '0';
-        char rule2 = '0';
+        boolean upper;
+        boolean rule1;
         
-        for(int i = 0; i < sentence.length(); i++) {
-        	char c = sentence.charAt(i);
+        if(sentence.length() == 1) {
+        	char c = sentence.charAt(0);
         	
-        	if(c >= 65 && c < 91) { // 대문자
-        		
-        		if(upper && rule1 == '0') {
-        			answer += (" "+c);
-        		}else {
-        			answer += c;
-        		}
-        		
+        	if(c < 91) { // 대문자
+        		return sentence;
+        	}else { // 소문자
+        		return "invalid";
+        	}
+        }else if(sentence.length() == 0) {
+        	return "";
+        }
+
+        while(sentence != "") {
+        	upper = false;
+        	rule1 = false;
+        	
+        	char c = sentence.charAt(0);
+        	
+        	if(c < 91) { // 대문자
         		upper = true;
         	}else { // 소문자
-        		
-        		if(rule1 != '0' && !upper) {
-        			return "invalid";
-        		}
-        		
-        		if(i == 0) {
-        			set.add(c);
-        			rule1 = c;
-        		}else {
-        			if(set.contains(c)) {
-            			if(rule1 == c) {
-            				rule1 = '0';
-            				rule2 = '0';
-
-            				if(i != sentence.length()-1) answer += " ";
-            				
-            			}else if(rule2 != c) {
-            				return "invalid";
-            			}
-            		}else {
-            			if(rule2 == '0') {
-            				set.add(c);
-            				rule2 = c;
-            			}else if(rule1 == '0') {
-            				set.add(c);
-            				rule1 = c;
-            				rule2 = '0';
-            				
-            				answer += " ";
-            			}
-            		}
-        		}
-        		
-        		upper = false;
+        		rule1 = true;
         	}
+        	
+        	if(rule1) { // rule1 시작
+        		int idx = 0;
+        		for(int i = 1; i < sentence.length(); i++) {
+        			if(c == sentence.charAt(i)) {
+        				idx = i;
+        				break;
+        			}
+        		}
+        		
+        		if(idx == 0) {
+        			return "invalid";
+        		}else {
+        			String temp = sentence.substring(0,idx+1);
+        			sentence = sentence.substring(idx+1);
+        			words.add(temp);
+        		}
+        	}else { // rule1 시작이 아닌 경우
+        		c = sentence.charAt(1);
+        		
+        		if(c < 91) { // 대문자
+        			String temp = sentence.substring(0,1);
+        			sentence = sentence.substring(1);
+        			words.add(temp);
+            	}else { // 소문자
+            		
+            	}
+        	}
+        	
         	
         }
         
