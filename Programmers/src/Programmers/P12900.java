6b8dcfbebdde21438a37425e857659a5fb7b5a4c
Seungho Lee <60536427+dltmdgh1994@users.noreply.github.com>
2021-09-06 18:11:54 +0900

    3 2 x n 타일링

---
+++ Programmers/src/Programmers/P12900.java
@@ -0,0 +1,24 @@
+package Programmers;
+
+public class P12900 { // 3 2 x n ≈∏¿œ∏µ
+
+	public static void main(String[] args) {
+		
+		P12900 p = new P12900();
+		
+		System.out.println(p.solution(4));
+	}
+	
+	public int solution(int n) {
+        
+        int[] dp = new int[n];
+        dp[0] = 1;
+        dp[1] = 2;
+        
+        for(int i = 2; i < n; i++) {
+        	dp[i] = (dp[i-1]+dp[i-2])%1000000007;
+        }
+        
+        return dp[n-1];
+    }
+}
