bfa868ae794ffc5518bb31b34c8c850d3560d295
Seungho Lee <60536427+dltmdgh1994@users.noreply.github.com>
2021-11-30 16:36:30 +0900

    3 길 찾기 게임

--- Programmers/src/Programmers/P42892.java
+++ Programmers/src/Programmers/P42892.java
@@ -7,16 +7,23 @@ public class P42892 { // 3 길 찾기 게임
 	public static void main(String[] args) {
 		
 		P42892 p = new P42892();
+		int[][] nodeinfo = {{5,3},{11,5},{13,3},{3,5},{6,1},{1,3},{8,6},{7,2},{2,2}};
 		
-		p.solution(null);
+		p.solution(nodeinfo);
 	}
 	
 	public int[][] solution(int[][] nodeinfo) {
-        int[][] answer = {{0,0},{1,1},{2,1}};
+        int[][] answer = new int[2][];
         
+        int[][] newNodeInfo = new int[nodeinfo.length][3];
+        for(int i = 0; i < nodeinfo.length; i++) {
+        	newNodeInfo[i][0] = nodeinfo[i][0];
+        	newNodeInfo[i][1] = nodeinfo[i][1];
+        	newNodeInfo[i][2] = i+1;
+        }
         
-        
-        Arrays.sort(nodeinfo, new Comparator<int[]>() {
+        // y 좌표를 내림차순, x 좌표를 오름차순으로 정렬
+        Arrays.sort(newNodeInfo, new Comparator<int[]>() {
 			@Override
 			public int compare(int[] o1, int[] o2) {
 				// TODO Auto-generated method stub
@@ -37,11 +44,24 @@ public class P42892 { // 3 길 찾기 게임
         	
 		});
         
-        for(int i = 0; i < answer.length; i++) {
-        	for(int j = 0; j < answer[0].length; j++) {
-        		System.out.print(answer[i][j] + " ");
-        	}
-        	System.out.println();
+        Tree tree = new Tree();
+        
+        for(int i = 0; i < newNodeInfo.length; i++) {
+        	int[] data = newNodeInfo[i];
+        	
+        	tree.insertNode(data);
+        }
+        
+        tree.preorderTree(tree.rootNode, 0);
+        answer[0] = new int[tree.preorderResult.size()];
+        for(int i = 0; i < tree.preorderResult.size(); i++) {
+        	answer[0][i] = tree.preorderResult.get(i);
+        }
+        
+        tree.postorderTree(tree.rootNode, 0);
+        answer[1] = new int[tree.postorderResult.size()];
+        for(int i = 0; i < tree.postorderResult.size(); i++) {
+        	answer[1][i] = tree.postorderResult.get(i);
         }
         
         return answer;
@@ -49,30 +69,67 @@ public class P42892 { // 3 길 찾기 게임
 }
 
 class Node {
-    int value;
-    int[] xy;
+    int[] data; // x,y,value
     Node leftChild;
     Node rightChild;
-    Node parent;
 
-    public Node(int value, int[] xy) {
-        this.value = value;
-        this.xy = xy;
+    public Node(int[] data) {
+        this.data = data;
         this.leftChild = null;
         this.rightChild = null;
-        this.parent = null;
     }
 }
 
 class Tree{
 	Node rootNode = null;
+	ArrayList<Integer> preorderResult = new ArrayList<>(); // 전위순회 결과
+	ArrayList<Integer> postorderResult = new ArrayList<>(); // 후위순회 결과
 	
-	public void insertNode(Node node) {
+	public void insertNode(int[] data) {
 		
-		if(this.rootNode == null) {
-			this.rootNode = node;
+		if(rootNode == null) {
+			rootNode = new Node(data);
 		}else {
+			Node head = rootNode;
+			Node currentNode;
 			
+			while(true) {
+				currentNode = head;
+				
+				if(head.data[0] > data[0]) {
+					head = head.leftChild;
+					
+					if(head == null) {
+						currentNode.leftChild = new Node(data);
+						break;
+					}
+				}else {
+					head = head.rightChild;
+					
+					if(head == null) {
+						currentNode.rightChild = new Node(data);
+						break;
+					}
+				}
+			}
 		}
 	}
+	
+	// 전위 순회
+    public void preorderTree(Node root, int depth) {
+        if (root != null) {
+        	preorderResult.add(root.data[2]);
+            preorderTree(root.leftChild, depth + 1);
+            preorderTree(root.rightChild, depth + 1);
+        }
+    }
+    
+    // 후위 순회
+    public void postorderTree(Node root, int depth) {
+        if (root != null) {
+            postorderTree(root.leftChild, depth + 1);
+            postorderTree(root.rightChild, depth + 1);
+            postorderResult.add(root.data[2]);
+        }
+    }
 }
