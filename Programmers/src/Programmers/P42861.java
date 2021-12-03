3e21eeb037a779817179de8e5e8c18494b4be5ef
Seungho Lee <60536427+dltmdgh1994@users.noreply.github.com>
2021-12-03 18:45:58 +0900

    3 섬 연결하기

---
+++ Programmers/src/Programmers/P42861.java
@@ -0,0 +1,18 @@
+package Programmers;
+
+public class P42861 { // º∂ ø¨∞·«œ±‚
+
+	public static void main(String[] args) {
+		
+		P42861 p = new P42861();
+
+	}
+	
+    public int solution(int n, int[][] costs) {
+        int answer = 0;
+        
+        
+        
+        return answer;
+    }
+}
