456653e9891febc9014500bf4628f8f230e95233
Seungho Lee <60536427+dltmdgh1994@users.noreply.github.com>
2022-01-10 18:22:40 +0900

    D7_BS

--- Programmers/src/etc/D7_BS.java
+++ Programmers/src/etc/D7_BS.java
@@ -1,6 +1,11 @@
 package etc;
 
+import java.io.*;
+
 public class D7_BS {
+	
+	BufferedReader br;
+	BufferedWriter bw;
 
 	public static void main(String[] args) {
 		
@@ -14,6 +19,8 @@ public class D7_BS {
 		
 		System.out.println(d.BS_recursion(arr, 0, 0, arr.length-1));
 		System.out.println(d.BS_for(arr, 100, 0, arr.length-1));
+		
+		d.D7_8();
 	}
 	
 	public int BS_recursion(int[] arr, int target, int start, int end) {
@@ -52,5 +59,58 @@ public class D7_BS {
 		}
 		
 		return -1; // Å½»ö ½ÇÆÐ
+	}
+	
+	public void D7_8(){
+		int answer = 0;
+		
+		try {
+			br = new BufferedReader(new InputStreamReader(System.in));
+			bw = new BufferedWriter(new OutputStreamWriter(System.out));
+			
+			String s[] = br.readLine().split(" ");
+			int n = Integer.parseInt(s[0]);
+			int len = Integer.parseInt(s[1]);
+			
+			s = br.readLine().split(" ");
+			int[] arr = new int[n];
+			int max = 0;
+			for(int i = 0; i < n; i++) {
+				arr[i] = Integer.parseInt(s[i]);
+				
+				if(arr[i] > max) {
+					max = arr[i];
+				}
+			}
+			
+			int start = 0;
+			int end = max;
+			int mid;
+			
+			while(start <= end) {
+				mid = (start+end)/2;
+				
+				int sum = 0;
+				for(int i = 0; i < n; i++) {
+					int l = arr[i] - mid;
+					if(l > 0) sum += l;
+				}
+				
+				if(sum < len) {
+					end = mid-1;
+				}else {
+					answer = mid;
+					start = mid+1;
+				}
+			}
+			
+			bw.write(Integer.toString(answer));
+
+			br.close();
+			bw.close();
+		}catch(IOException e) {
+			e.printStackTrace();
+			System.out.println(e.getMessage());
+		}
 	}
 }
