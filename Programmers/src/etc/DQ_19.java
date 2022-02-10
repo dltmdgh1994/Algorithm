8bce75c2b5154382e7242b6da3bc5eaac27196c3
Seungho Lee <60536427+dltmdgh1994@users.noreply.github.com>
2022-02-10 18:11:37 +0900

    Update DQ_19.java

--- Programmers/src/etc/DQ_19.java
+++ Programmers/src/etc/DQ_19.java
@@ -7,7 +7,7 @@ public class DQ_19 {
 
 	BufferedReader br;
 	BufferedWriter bw;
-	ArrayList<String> allOp;
+	ArrayList<char[]> allOp;
 	
 	public static void main(String[] args) {
 		
@@ -54,6 +54,21 @@ public class DQ_19 {
 			perm(op, visited, 0, "");
 			
 			// 계산하기
+			int max = Integer.MIN_VALUE;
+			int min = Integer.MAX_VALUE;
+			for(int i = 0; i < allOp.size(); i++) {
+				char[] operator = allOp.get(i);
+				
+				// int result = cal1(arr, operator);
+				int result = cal2(arr, operator);
+				
+				if(result > max) max = result;
+				if(result < min) min = result;
+			}
+			
+			bw.write(Integer.toString(max));
+			bw.newLine();
+			bw.write(Integer.toString(min));
 			
 			br.close();
 			bw.close();
@@ -63,10 +78,11 @@ public class DQ_19 {
 		}
 	}
 	
+	// 연산자 순열 생성
 	private void perm(String op, boolean[] visited, int depth, String tmp) {
 		
 		if(depth == op.length()) {
-			allOp.add(tmp);
+			allOp.add(tmp.toCharArray());
 			System.out.println(tmp);
 			return;
 		}
@@ -78,5 +94,72 @@ public class DQ_19 {
 				visited[i] = false;
 			}
 		}
+	}
+	
+	// 연산자의 우선순위를 고려한 경우
+	private int cal1(int[] arr, char[] operator) {
+		
+		ArrayList<Integer> tmpArr = new ArrayList<>();
+		for(int i = 0; i < arr.length; i++) {
+			tmpArr.add(arr[i]);
+		}
+		
+		ArrayList<Character> tmpOp = new ArrayList<>();
+		for(int i = 0; i < operator.length; i++) {
+			tmpOp.add(operator[i]);
+		}
+		
+		int cnt = 0;
+		for(int i = 0; i < operator.length; i++) {
+			char c = tmpOp.get(cnt);
+			if(c == 'x') {
+				tmpArr.set(cnt, tmpArr.get(cnt)*tmpArr.get(cnt+1));
+				tmpArr.remove(cnt+1);
+				tmpOp.remove(cnt);
+			}else if(c == '/') {
+				tmpArr.set(cnt, tmpArr.get(cnt)/tmpArr.get(cnt+1));
+				tmpArr.remove(cnt+1);
+				tmpOp.remove(cnt);
+			}else {
+				cnt++;
+			}
+		}
+		
+		while(!tmpOp.isEmpty()) {
+			char c = tmpOp.get(0);
+			if(c == '+') {
+				tmpArr.set(0, tmpArr.get(0)+tmpArr.get(1));
+				tmpArr.remove(1);
+				tmpOp.remove(0);
+			}else if(c == '-') {
+				tmpArr.set(0, tmpArr.get(0)-tmpArr.get(1));
+				tmpArr.remove(1);
+				tmpOp.remove(0);
+			}
+		}
+		
+		return tmpArr.get(0);
+	}
+	
+	// 연산자의 우선순위를 고려하지 않은 경우
+	private int cal2(int[] arr, char[] operator) {
+		
+		int result = arr[0];
+		for(int i = 0; i < operator.length; i++) {
+			char op = operator[i];
+			int num = arr[i+1];
+			
+			if(op == '+') {
+				result += num;
+			}else if(op == '-') {
+				result -= num;
+			}else if(op == 'x') {
+				result *= num;
+			}else {
+				result /= num;
+			}
+		}
+		
+		return result;
 	}
 }
