4fc5cf5d4818252dcd903bf2647a140e93f867d7
Seungho Lee <60536427+dltmdgh1994@users.noreply.github.com>
2022-02-10 20:47:19 +0900

    Create DQ_20.java

---
+++ Programmers/src/etc/DQ_20.java
@@ -0,0 +1,136 @@
+package etc;
+
+import java.io.*;
+
+public class DQ_20 { // 감시 피하기
+	
+	BufferedReader br;
+	BufferedWriter bw;
+	
+	int n;
+	int[][] map;
+	int[][] tmp;
+	boolean ans = false;
+
+	public static void main(String[] args) {
+		
+		DQ_20 dq = new DQ_20();
+		
+		dq.sol();
+	}
+	
+	public void sol() {
+		
+		try {
+			br = new BufferedReader(new InputStreamReader(System.in));
+			bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+			String[] s = br.readLine().split(" ");
+			int n = Integer.parseInt(s[0]);
+			
+			int[][] map = new int[n][n];
+
+			for(int i = 0; i < n; i++) {
+				s = br.readLine().split(" ");
+				for(int j = 0; j < n; j++) {
+					if(s[i].equals("X")) {
+						map[i][j] = 0;
+					}else if(s[i].equals("S")) {
+						map[i][j] = 1;
+					}else {
+						map[i][j] = 2;
+					}
+				}
+			}
+			
+			dfs(0);
+			
+			if(ans) {
+				bw.write("YES");
+			}else {
+				bw.write("NO");
+			}
+			
+			br.close();
+			bw.close();
+		}catch(IOException e) {
+			e.printStackTrace();
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	// 벽을 3개까지 설치 가능하므로 모든 경우를 고려
+	private void dfs(int cnt) {
+		
+		if(cnt == 3) {
+			for(int i = 0; i < n; i++) {
+				for(int j = 0; j < n; j++) {
+					tmp[i][j] = map[i][j];
+				}
+			}
+			
+			boolean isStudent = false;
+			for(int i = 0; i < n; i++) {
+				for(int j = 0; j < n; j++) {
+					if(!check(i,j)) {
+						isStudent = true;
+					}
+				}
+			}
+			
+			if(!isStudent) ans = true;
+			
+			return;
+		}
+		
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < n; j++) {
+				if(map[i][j] == 0) {
+					map[i][j] = 3;
+					cnt++;
+					dfs(cnt);
+					cnt--;
+					map[i][j] = 0;
+				}
+			}
+		}
+	}
+	
+	// 학생 확인
+	private boolean check(int x, int y) {
+		
+		for(int i = x+1; i < n; i++) {
+			if(tmp[i][y] == 1) {
+				return false;
+			}else if(tmp[i][y] == 3) {
+				break;
+			}
+		}
+		
+		for(int i = x-1; i >= 0; i--) {
+			if(tmp[i][y] == 1) {
+				return false;
+			}else if(tmp[i][y] == 3) {
+				break;
+			}
+		}
+		
+		for(int i = y+1; i < n; i++) {
+			if(tmp[x][i] == 1) {
+				return false;
+			}else if(tmp[x][i] == 3) {
+				break;
+			}
+		}
+		
+		for(int i = y-1; i >= 0; i--) {
+			if(tmp[x][i] == 1) {
+				return false;
+			}else if(tmp[x][i] == 3) {
+				break;
+			}
+		}
+		
+		return true;
+	}
+}
